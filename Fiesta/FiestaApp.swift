31825d85f786e528148435cdcd9e83f22919edf7
fredycamas <fcamas62@gmail.com>
2023-03-01 23:05:19 -0500

    Initial commit

---
+++ Fiesta/FiestaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiestaApp.swift
+//  Fiesta
+//
+//  Created by Life on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FiestaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
